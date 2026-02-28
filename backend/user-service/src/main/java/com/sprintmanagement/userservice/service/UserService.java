7e2a1dca874b788cdf94a1192dd839d551399aaf
Yuvraj Singh <uvi58493@gmail.com>
2026-02-28 10:16:33 +0530

    feat: implement UserService class for user management

---
+++ backend/user-service/src/main/java/com/sprintmanagement/userservice/service/UserService.java
@@ -0,0 +1,15 @@
+package com.sprintmanagement.userservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserService userService;
+
+    public UserService(UserService userService) {
+        this.userService = userService;
+    }
+
+
+}

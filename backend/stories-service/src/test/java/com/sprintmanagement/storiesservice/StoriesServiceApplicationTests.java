4b788aae5102063d8ac72dc7a46a77efeff92f84
Yuvraj Singh <uvi58493@gmail.com>
2026-03-02 12:54:22 +0530

    feat: add unit tests for StoriesServiceApplication context loading

---
+++ backend/stories-service/src/test/java/com/sprintmanagement/storiesservice/StoriesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintmanagement.storiesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoriesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

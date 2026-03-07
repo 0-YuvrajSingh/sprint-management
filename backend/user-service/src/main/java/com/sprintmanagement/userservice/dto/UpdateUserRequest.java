a811df88213682f47e19edfacb68849cd318f2a0
Yuvraj Singh <uvi58493@gmail.com>
2026-03-07 09:57:26 +0530

    Remove password field from UpdateUserRequest DTO

--- backend/user-service/src/main/java/com/sprintmanagement/userservice/dto/UpdateUserRequest.java
+++ backend/user-service/src/main/java/com/sprintmanagement/userservice/dto/UpdateUserRequest.java
@@ -11,9 +11,6 @@ public class UpdateUserRequest {
     @Email(message = "Email must be a valid address")
     private String email;
 
-    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
-    private String password;
-
     public UpdateUserRequest() {
     }
 
@@ -26,19 +23,11 @@ public class UpdateUserRequest {
         return email;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
